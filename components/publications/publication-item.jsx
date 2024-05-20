08b00ed3151e77d27823944602852acf7ddd99c6
Edward Chen <edwjchen@gmail.com>
2024-05-20 13:36:22 -0400

    changed publication margins

--- components/publications/publication-item.jsx
+++ components/publications/publication-item.jsx
@@ -7,7 +7,7 @@ import {faGithub} from '@fortawesome/free-brands-svg-icons'
 
 function PublicationItem(props) {
     return (
-        <div className="group flex flex-row mb-4 transition-all">
+        <div className="group flex flex-row mb-12 transition-all">
             <div>
                 <a href={props.href} target="_blank" rel="noopener noreferrer" className='font-medium transition-all hover:text-primary-500'>{props.title}</a>
                 <div className='mb-2 text-primary-400'>{props.venue} | {props.year}</div>
