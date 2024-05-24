277e82eb36b7f6fb07d8fdac1674d922aa7c17da
orestischar <orestischar@gmail.com>
2024-05-24 18:58:12 -0400

    bold

--- components/about.jsx
+++ components/about.jsx
@@ -5,7 +5,7 @@ const About = (props) => {
         <div data-section id='about' className='mb-16 group'>
             <div className='text-surface-600'>
                 <div className='mb-6 text-primary-300'>Hi, I'm Orestis!</div>
-                <div className='mb-6'>I am a grad student in CMU, graduating in August with a MS in Computer Science Research. My research was focused on theoretical and applied cryptography, with a focus on quantum cryptography and zero-knowledge proof systems. I am actively seeking opportunities in security and computer science, where I can apply my expertise and passion for technology to innovative projects.
+                <div className='mb-6'>I am a grad student in CMU, graduating in August with a MS in Computer Science Research. My research was focused on theoretical and applied cryptography, with a focus on <strong>quantum cryptography</strong> and <strong>zero-knowledge proof systems</strong>. I am actively seeking opportunities in security and computer science, where I can apply my expertise and passion for technology to innovative projects.
  </div>
             </div>
         </div>
